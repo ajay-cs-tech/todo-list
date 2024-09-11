89707cb76ee1d111dac8a17b6a73b11fe2e9b787
Ajay CS <71887972+ajaycs91333@users.noreply.github.com>
2024-09-11 17:14:13 +0530

    new

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react"
-import TodoInput from "./components/Todoinput"
-import TodoList from "./components/Todolist"
+import TodoInput from "./components/TodoInput"
+import TodoList from "./components/TodoList"
 
 function App() {
   const [todos, setTodos] = useState([])
